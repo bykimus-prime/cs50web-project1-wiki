eb4b21685b7244f53023e72ef2944bc8bcdc3154
Alexander Rodzyanko <alexander.rodzyanko@gmail.com>
2022-12-24 19:01:19 +0900

    Create search results page and finish function to append results to page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -33,7 +33,16 @@ def search(request):
       entry_search = request.POST['q']
       html_content = convert_md_to_html(entry_search)
       if html_content is not None:
-            return render(request, "encyclopedia/entry.html", {
-               "title": entry_search,
-               "content": html_content
+         return render(request, "encyclopedia/entry.html", {
+            "title": entry_search,
+            "content": html_content
+         })
+      else:
+         allEntries = util.list_entries()
+         recommendation = []
+         for entry in allEntries:
+            if entry_search.lower() in entry.lower():
+               recommendation.append(entry)
+            return render(request, "encyclopedia/search.html", {
+               "recommendation": recommendation
             })
