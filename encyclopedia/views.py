06d3500ca989bc6f4a4370a27219cc9819366808
Alexander Rodzyanko <alexander.rodzyanko@gmail.com>
2022-12-24 18:55:03 +0900

    Create search function to give results for pages we already have

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -27,3 +27,13 @@ def entry(request, title):
          "title": title,
          "content": html_content
       })
+
+def search(request):
+   if request.method == "POST":
+      entry_search = request.POST['q']
+      html_content = convert_md_to_html(entry_search)
+      if html_content is not None:
+            return render(request, "encyclopedia/entry.html", {
+               "title": entry_search,
+               "content": html_content
+            })
