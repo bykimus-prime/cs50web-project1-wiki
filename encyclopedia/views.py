93392bcd6e6f61e692d96408d2991ab2405d90bf
Alexander Rodzyanko <alexander.rodzyanko@gmail.com>
2022-12-29 12:54:02 +0900

    Add ability to save newly created page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -50,3 +50,18 @@ def search(request):
 def new_page(request):
    if request.method == "GET":
       return render(request, "encyclopedia/newpage.html")
+   else:
+      title = request.POST['title']
+      content = request.POST['content']
+      titleExist = util.get_entry(title)
+      if titleExist is not None:
+         return render(request, "encyclopedia/error.html", {
+            "message": "Entry page already exists."
+         })
+      else:
+         util.save_entry(title, content)
+         html_content = convert_md_to_html(title)
+         return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+         })
